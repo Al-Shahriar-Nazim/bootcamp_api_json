192f7de27707965f16261b1bcfaeaf37d73badcb
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-10 00:09:07 +0600

    again display and styling

--- api.js
+++ api.js
@@ -19,22 +19,45 @@ const postData = () => {
     });
 };
 
+// const displayData = (posts) => {
+//   //1.get the container
+//   const postContainer = document.getElementById("post-container");
+//   postContainer.innerHTML ="";
+//   // console.log(postContainer);
+
+//   posts.forEach((post) => {
+//     // console.log(post);
+//     // console.log(post.title);
+
+//     //2.create html element
+//     const li = document.createElement("li");
+//     li.innerText = post.title;
+//     console.log(li)
+
+//     //3.append the element to the container
+//     postContainer.appendChild(li);
+//   });
+// };
+
 const displayData = (posts) => {
-  //1.get the container
+  //1. get the container and empty the container
   const postContainer = document.getElementById("post-container");
-  postContainer.innerHTML ="";
-  // console.log(postContainer);
+  postContainer.innerHTML = "";
 
   posts.forEach((post) => {
-    // console.log(post);
-    // console.log(post.title);
+    // console.log(post)
+    // 2. create html element
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="post-card">
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+      </div>
+    `;
 
-    //2.create html element
-    const li = document.createElement("li");
-    li.innerText = post.title;
-    console.log(li)
-
-    //3.append the element to the container
-    postContainer.appendChild(li);
+    //3. append the element to the container
+    postContainer.appendChild(div);
   });
 };
+
+postData()
