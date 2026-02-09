65b42c5010976a8017570ebf04867ded3e90a571
Al-Shahriar-Nazim <nazimking7634@gmail.com>
2026-02-09 23:30:31 +0600

    added post data loaded

--- api.js
+++ api.js
@@ -1,12 +1,26 @@
-let dataLoad =()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-
-}
+let dataLoad = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
 
 // function click(){
 //   fetch("https://jsonplaceholder.typicode.com/todos/1")
 //     .then((res) => res.json())
 //     .then((data) => console.log(data));
 // }
+
+const postData = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      displayData(data);
+    });
+};
+
+const displayData = (posts) => {
+  posts.forEach((post) => {
+    console.log(post);
+  });
+};
